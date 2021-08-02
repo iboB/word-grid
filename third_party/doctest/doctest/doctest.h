1f9398a0ac784bb33e18d75aab150fe5c123bb9a
Borislav Stanimirov <b.stanimirov@abv.bg>
2021-08-02 16:40:09 +0300

    Removed third_party and submodules. About to move to cpm

--- third_party/doctest/doctest/doctest.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "doctest_fwd.h"
-
-#if defined(DOCTEST_CONFIG_IMPLEMENT)
-#   include "doctest_impl.h"
-#endif
--- third_party/doctest/doctest/doctest_main.cpp
+++
@@ -1,3 +0,0 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest/doctest_fwd.h"
-#include "doctest/doctest_impl.h"

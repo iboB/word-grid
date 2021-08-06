057d1fba25a1b5bf32d67a6063b0e6f748de0615
Borislav Stanimirov <b.stanimirov@abv.bg>
2021-08-06 07:32:11 +0300

    Moved (simplified) board to core

---
+++ core/code/core/Board.cpp
@@ -0,0 +1,23 @@
+// word-grid
+// Copyright (c) 2019 Borislav Stanimirov
+//
+// Distributed under the MIT Software License
+// See accompanying file LICENSE.txt or copy at
+// https://opensource.org/licenses/MIT
+//
+#include "Board.hpp"
+
+namespace core
+{
+
+Board::Board(Grid&& grid, ScoredDictionary&& dictionary)
+    : m_grid(std::move(grid))
+    , m_dictionary(std::move(dictionary))
+{}
+
+Board::~Board() = default;
+
+Board::Board(Board&& other) noexcept = default;
+Board& Board::operator=(Board&&) noexcept = default;
+
+}
---
+++ core/code/core/Board.hpp
@@ -0,0 +1,39 @@
+// word-grid
+// Copyright (c) 2019 Borislav Stanimirov
+//
+// Distributed under the MIT Software License
+// See accompanying file LICENSE.txt or copy at
+// https://opensource.org/licenses/MIT
+//
+#pragma once
+
+#include "Grid.hpp"
+#include "ScoredDictionary.hpp"
+
+#include <unordered_map>
+
+namespace core
+{
+class Board
+{
+public:
+    Board(Grid&& grid, ScoredDictionary&& dic);
+    ~Board();
+
+    Board(const Board&) = delete;
+    Board& operator=(const Board&) = delete;
+
+    Board(Board&&) noexcept;
+    Board& operator=(Board&&) noexcept;
+
+    const Grid& grid() const { return m_grid; }
+    const ScoredDictionary& dictionary() const { return m_dictionary; }
+
+private:
+    Grid m_grid;
+
+    // words which are found in the grid of this board
+    // this is NOT the complete game dictionary
+    ScoredDictionary m_dictionary;
+};
+}

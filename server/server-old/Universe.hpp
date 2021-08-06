d6878badb3ef346779e8499c8e8785e5413a4892
Borislav Stanimirov <b.stanimirov@abv.bg>
2021-08-06 07:33:19 +0300

    Copyright years

--- server/server-old/Universe.hpp
+++ server/server-old/Universe.hpp
@@ -1,5 +1,5 @@
 // word-grid
-// Copyright (c) 2019 Borislav Stanimirov
+// Copyright (c) 2019-2021 Borislav Stanimirov
 //
 // Distributed under the MIT Software License
 // See accompanying file LICENSE.txt or copy at

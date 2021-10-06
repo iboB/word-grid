f49536ffa9119c39308371c6f92e0c5864e077b3
Borislav Stanimirov <b.stanimirov@abv.bg>
2021-10-06 14:56:12 +0300

    Experimenting with random generated grid

--- scratch/scratch-b.cpp
+++ scratch/scratch-b.cpp
@@ -1,8 +1,70 @@
 #include <iostream>
 
+#include <core/LanguageBuilder.hpp>
+#include <core/LetterSequenceFromUtf8.hpp>
+#include <core/PRNG.hpp>
+#include <core/Grid.hpp>
+
 using namespace std;
 
-int main() {
-    cout << "lol\n";
+core::GridElement ab(std::string_view str, core::score_t s)
+{
+    core::GridElement ret;
+    core::LetterSequence_FromUtf8(ret, str);
+    ret.setScore(s);
+    return ret;
+}
+
+int main()
+{
+    core::LanguageBuilder lb;
+    lb.setAlphabet({
+        ab("a", 2), //
+        ab("b", 5), //
+        ab("c", 3), //
+        ab("d", 3), //
+        ab("e", 1), //
+        ab("f", 5), //
+        ab("g", 4), //
+        ab("h", 4), //
+        ab("i", 2), //
+        ab("j", 9), //
+        ab("k", 6), //
+        ab("l", 3), //
+        ab("m", 4), //
+        ab("n", 2), //
+        ab("o", 2), //
+        ab("p", 4), //
+        ab("q", 10), //
+        ab("r", 2), //
+        ab("s", 2), //
+        ab("t", 2), //
+        ab("u", 4), //
+        ab("v", 6), //
+        ab("w", 6), //
+        ab("x", 9), //
+        ab("y", 5), //
+        ab("z", 8), //
+    });
+    auto lang = lb.getLanguage();
+
+    core::Grid g(4, 4);
+
+    core::PRNG rng;
+    auto& ft = lang.alphabetFrequencyTable();
+    for (auto& e : g.elements())
+    {
+        e = rng.randomElement(ft);
+    }
+
+    for (uint8_t y = 0; y < g.h(); ++y)
+    {
+        for (uint8_t x = 0; x < g.w(); ++x)
+        {
+            cout << char(g[core::GridCoord{x, y}][0]);
+        }
+        cout << '\n';
+    }
+
     return 0;
 }

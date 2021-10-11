4c7d372f13c5d443ce93ad01fca4e81bb7d51118
Borislav Stanimirov <b.stanimirov@abv.bg>
2021-10-11 11:47:09 +0300

    Increased max size of grid element to 5 so as to capture over- and -tion

--- core/code/core/GridElementTraits.hpp
+++ core/code/core/GridElementTraits.hpp
@@ -15,7 +15,7 @@ namespace core
 struct GridElementTraits
 {
     static constexpr size_t Min_Length = 1;
-    static constexpr size_t Max_Length = 4;
+    static constexpr size_t Max_Length = 5;
 };
 
 } // namespace core

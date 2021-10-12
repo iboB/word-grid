33eec4d88c0fab17fa11e3c192787939c3a13470
Borislav Stanimirov <b.stanimirov@abv.bg>
2021-10-12 18:13:00 +0300

    More experiments

--- scratch/scratch-b.cpp
+++ scratch/scratch-b.cpp
@@ -2,11 +2,14 @@
 
 #include "PlatformUtil.hpp"
 
+#include <core/BasicBoard.hpp>
+#include <core/BoardUtils.hpp>
+#include <core/Grid.hpp>
+#include <core/GridUtils.hpp>
 #include <core/LanguageBuilder.hpp>
 #include <core/LetterSequenceFromUtf8.hpp>
 #include <core/PRNG.hpp>
-#include <core/Grid.hpp>
-#include <core/GridUtils.hpp>
+#include <core/ScoreUtils.hpp>
 #include <core/ScoredWord.hpp>
 
 #include <set>
@@ -72,16 +75,12 @@ int main()
         ab("y", 5), //
         ab("z", 8), //
     });
-    lb.setConversionTable({
-        {'-', {}},
-        {'\'', {}},
-        {241, core::LetterSequence_FromUtf8<core::LetterConversionTarget>("n")}
-    });
+    lb.setConversionTable({{'-', {}}, {'\'', {}}, {241, core::LetterSequence_FromUtf8<core::LetterConversionTarget>("n")}});
     lb.setMinWordLength(3);
 
     auto assetPath = core::PlatformUtil::getAssetPath(core::PlatformUtil::getModulePath(), "assets");
     auto dicPath = assetPath + "/dictionaries/sowpods+12d.txt";
-    //auto dicPath = assetPath + "/dictionaries/12dicts.txt";
+    // auto dicPath = assetPath + "/dictionaries/12dicts.txt";
     auto buf = readFile(dicPath.c_str());
     lb.setDictionaryUtf8Buffer(buf);
 
@@ -98,89 +97,132 @@ int main()
     }
     auto& lang = *el;
 
-    core::Grid g({4, 4});
+    core::BasicBoard bb = {core::Grid({4, 4}), {}};
 
-    auto rseed = core::PRNG::randomDevice();
+    // auto rseed = core::PRNG::randomDevice();
+    auto rseed = 1998847862;
     cout << "Seeding with: " << rseed << '\n';
     core::PRNG rng(rseed);
 
     auto addWord = [&](std::string_view str) {
-        auto p = core::impl::generateRandomEmptyPath(str.length(), g, rng);
+        auto p = core::impl::generateRandomEmptyPath(str.length(), bb.grid, rng);
         if (p.empty()) return false;
-        for (size_t i = 0; i < str.length(); ++i)
-        {
-            g[p[i]] = lang.alphabet()[str[i] - 'a'];
-        }
+        for (size_t i = 0; i < str.length(); ++i) { bb.grid[p[i]] = lang.alphabet()[str[i] - 'a']; }
 
         return true;
     };
 
-    //std::vector<std::string_view> seedWords = {"ape", "horse", "cow"};
-    //for (auto w : seedWords)
+    // int success = -1;
+    // for (int i = 0; i < 1000; ++i)
     //{
-    //    addWord(w);
+    //    // std::vector<std::string_view> seedWords = {"ape", "horse", "cow"};
+    //    // for (auto w : seedWords)
+    //    //{
+    //    //    addWord(w);
+    //    //}
+    //    while (true)
+    //    {
+    //       auto& word = rng.randomElement(lang.mediumWords()).get();
+    //       if (addWord(word.displayString)) cout << "added " << word.displayString << '\n';
+    //       else break;
+    //    }
+
+    //    auto& ft = lang.alphabetFrequencyTable();
+    //    for (auto& e : bb.grid.elements())
+    //    {
+    //        if (e.empty()) e = rng.randomElement(ft);
+    //    }
+
+    //    bb.words = core::impl::findAllWordsInGridTmp(bb.grid, lang.dictionary());
+
+    //    for (auto& w : bb.words) { w.score = core::impl::classicScore(w, bb.grid); }
+
+    //    if (!core::impl::scheckAllCellsUsedTwice(bb))
+    //    {
+    //        bb.grid.clear();
+    //        continue;
+    //    }
+
+    //    if (!core::impl::scheckDupWordsSameScore(bb))
+    //    {
+    //        bb.grid.clear();
+    //        cout << "uh-oh\n";
+    //        continue;
+    //    }
+
+    //    success = i;
+    //    break;
     //}
-    while (true)
+
+    // if (success == -1)
+    //{
+    //    cout << "fail\n";
+    //    return 0;
+    //}
+
+    // cout << "success on iteration " << success << "\n";
+
+    std::string_view gridData =
+        // "gore"
+        // "aapr"
+        // "chto"
+        // "osif";
+        "nori"
+        "hgut"
+        "wahp"
+        "resy";
+    // "dona"
+    // "awer"
+    // "isef"
+    // "reso";
+
+    for (uint32_t i = 0; i < 16; ++i)
     {
-       auto& word = rng.randomElement(lang.commonWordsByLength()).get();
-       if (addWord(word.displayString)) cout << "added " << word.displayString << '\n';
-       else break;
-    }
-
-    auto& ft = lang.alphabetFrequencyTable();
-    for (auto& e : g.elements())
-    {
-       if (e.score() == 0) e = rng.randomElement(ft);
-    }
-
-    // std::string_view gridData =
-    //     // "gore"
-    //     // "aapr"
-    //     // "chto"
-    //     // "osif";
-    //     "nori"
-    //     "hgut"
-    //     "wahp"
-    //     "resy";
-    //     // "dona"
-    //     // "awer"
-    //     // "isef"
-    //     // "reso";
-
-    // for (uint32_t i = 0; i < 16; ++i)
-    // {
-    //     g[i].push_back(gridData[i]);
-    // }
-
-    for (uint8_t y = 0; y < g.dim().h; ++y)
-    {
-        for (uint8_t x = 0; x < g.dim().w; ++x)
+        auto c = gridData[i];
+        auto& ab = lang.alphabet();
+        for (auto& a : ab)
         {
-            cout << char(g[core::GridCoord{x, y}][0]);
+            if (a.front() == c)
+            {
+                bb.grid[i] = a;
+                break;
+            }
         }
+    }
+    bb.words = core::impl::findAllWordsInGridTmp(bb.grid, lang.dictionary());
+    for (auto& w : bb.words) { w.score = core::impl::classicScore(w, bb.grid); }
+
+    for (uint8_t y = 0; y < bb.grid.dim().h; ++y)
+    {
+        for (uint8_t x = 0; x < bb.grid.dim().w; ++x) { cout << char(bb.grid[core::GridCoord{x, y}][0]); }
         cout << '\n';
     }
 
-    auto words = core::impl::findAllWordsInGridTmp(g, lang.dictionary());
+    itlib::flat_map<std::string_view, core::score_t> common;
+    core::score_t commonScore = 0;
+    itlib::flat_map<std::string_view, core::score_t> uncommon;
+    core::score_t uncommonScore = 0;
 
-    std::set<std::string_view> common;
-    std::set<std::string_view> uncommon;
-
-    for (auto& w : words) {
-        if (w.uncommon) uncommon.insert(w.displayString);
-        else common.insert(w.displayString);
+    for (auto& w : bb.words)
+    {
+        if (w.uncommon)
+        {
+            uncommonScore += w.score;
+            uncommon[w.displayString] = w.score;
+        }
+        else
+        {
+            commonScore += w.score;
+            common[w.displayString] = w.score;
+        }
     }
 
-    cout << "Found " << (common.size() + uncommon.size()) << " words\n";
+    cout << "Found " << (common.size() + uncommon.size()) << " words. Total score: " << (commonScore + uncommonScore) << "\n";
 
-    cout << "\n\ncommon (" << common.size() << "): ";
-    for (auto& w : common) {
-        cout << w << ", ";
-    }
-    cout << "\n\nuncommon (" << uncommon.size() << "): ";
-    for (auto& w : uncommon) {
-        cout << w << ", ";
-    }
+    cout << "\n\nCommon words: " << common.size() << ". Score: " << commonScore << '\n';
+    for (auto& w : common) { cout << w.first << '(' << w.second << "), "; }
+    cout << "\n\nUncommon words: " << uncommon.size() << ". Score: " << uncommonScore << '\n';
+    for (auto& w : uncommon) { cout << w.first << '(' << w.second << "), "; }
 
     cout << endl;
 

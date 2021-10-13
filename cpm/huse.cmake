3d005ee2bedbc0df823e763b2b3a28c30d29ceeb
Borislav Stanimirov <b.stanimirov@abv.bg>
2021-10-13 10:31:35 +0300

    Added package huse and related updates

---
+++ cpm/huse.cmake
@@ -0,0 +1,5 @@
+CPMAddPackage(
+    NAME huse
+    GITHUB_REPOSITORY iboB/huse
+    VERSION 0.1.0
+)
--- cpm/icm.cmake
+++ cpm/icm.cmake
@@ -1,5 +1,5 @@
 CPMAddPackage(
     NAME icm
     GITHUB_REPOSITORY iboB/icm
-    VERSION 1.3.1
+    VERSION 1.3.2
 )
